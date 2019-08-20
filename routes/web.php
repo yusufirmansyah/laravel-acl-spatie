eb5b3d60af90051677e08fb431ac658e3738dac4
yusufirmansyah <yusuf.firmansyah@gmail.com>
2019-08-20 21:30:15 +0300

    Assign, remove, give, revoke Role Permission and permission via role

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,48 @@
 |
 */
 
+use Spatie\Permission\Models\Role;
+
 Route::get('/', function () {
     return view('welcome');
+
+    // ! Roles
+    // auth()->user()->assignRole('user');
+    // auth()->user()->removeRole('admin');
+    // auth()->user()->assignRole('admin', 'user');
+    // auth()->user()->syncRoles('admin');
+    // auth()->user()->syncRoles(['admin']);
+
+    // ! Permissions
+    // $user = auth()->user();
+
+    // $user->givePermissionTo('add post');
+    // $user->givePermissionTo('edit post');
+    // $user->revokePermissionTo('edit post');
+    // $user->syncPermissions(['add post', 'delete post']);
+    // dd($user->hasPermissionTo('edit post'));
+    // dd($user->hasAnyPermission(['edit post', 'delete post']));
+
+    // ! Role has Permissions
+    // $role = Role::find(1);
+
+    // $role->givePermissionTo('add post', 'edit post', 'delete post', 'view post');
+    // $role->revokePermissionTo('edit post');
+    // $role->syncPermissions(['add post', 'edit post', 'delete post', 'view post']);
+    // dd($role->hasPermissionTo('edit post'));
+    // dd($role->hasAnyPermission(['edit post', 'delete post']));
+
+    // ! cek permission dari User yang sedang login
+    // $user = auth()->user();
+
+    // dd($user->can('add post'));
+
+    // ! memberi permission khusus (direct permission) pada User selain permission dari Role-nya
+    // $user = auth()->user();
+
+    // $user->givePermissionTo('add post');
+
+    // dd($user->can('add post'));
 });
 
 Auth::routes();
